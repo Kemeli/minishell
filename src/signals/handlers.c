f3b769f8e15b9a651cbdb0be868a8739313c9334
Kaio Matos <kaio_matos22@hotmail.com>
2023-03-09 22:04:20 -0300

    fix: throw \n to stderr

--- src/signals/handlers.c
+++ src/signals/handlers.c
@@ -3,7 +3,7 @@
 void	handle_sigint(int sig)
 {
 	(void)sig;
-	ft_putchar_fd('\n', STDOUT_FILENO);
+	ft_putchar_fd('\n', STDERR_FILENO);
 	rl_replace_line("", 0);
 	rl_on_new_line();
 	rl_redisplay();
