6c0d25715b085d87a62d5090f376321c71703fff
kdaine- <kdaiane-@student.42sp.org.br>
2023-03-22 21:47:38 +0100

    quotes inside quotes with simple cmd

--- src/parser/lexer_utils.c
+++ src/parser/lexer_utils.c
@@ -71,40 +71,58 @@ char	*handle_dollar(char *input)
 	return (ret);
 }
 
+char	*cpy_character(char *str, char *new, int i)
+{
+	char	*chr;
+	char	*temp;
+
+	if (str[i])
+	{
+		chr = ft_substr(str, i, 1);
+		temp = ft_strjoin(new, chr);
+		free (chr);
+		free (new);
+		new = ft_strdup(temp);
+		free (temp);
+	}
+	return (new);
+}
+
 static char	*cut_quotes(char *str)
 {
 	int		i;
-	char	*chr;
 	char	*new;
-	char	*temp;
+	int		db_quotes;
+	int		sp_quotes;
 
 	i = 0;
-	if (!(ft_strchr(str, '\'') || ft_strchr(str, '\"')))
-		return (str);
+	db_quotes = 0;
+	sp_quotes = 0;
 	new = ft_calloc (sizeof(char *), 1);
 	while (str[i])
 	{
-		while (str[i] && ft_strchr("\"\'", str[i]))
-			i++;
-		if (str && str[i])
-		{
-			chr = ft_substr(str, i, 1);
-			temp = ft_strjoin(new, chr);
-			free (chr);
-			free (new);
-			new = ft_strdup(temp);
-			free (temp);
+		if (str[i] == '\"' && !sp_quotes)
+		{	db_quotes = !db_quotes;
 			i++;
 		}
+		if (str[i] == '\'' && !db_quotes)
+		{
+			sp_quotes = !sp_quotes;
+			i++;
+		}
+		new = cpy_character(str, new, i);
+		if (str[i])
+			i++;
 	}
+	free (str);
 	return (new);
 }
 
 char	*handle_quotes_dollar(char *input)
 {
+	if (input && (ft_strchr(input, '\'') || ft_strchr(input, '\"')))
+		input = cut_quotes (input);
 	if (input && ft_strchr("$", input[0]))
 		input = handle_dollar(input);
-	if (input)
-		input = cut_quotes (input);
 	return (input);
 }
