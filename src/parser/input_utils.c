c97a19f03781e08d8e9a9db60df46a49e07029ad
kdaine- <kdaiane-@student.42sp.org.br>
2023-03-23 01:36:27 +0100

    fix: bug quotes

--- src/parser/input_utils.c
+++ src/parser/input_utils.c
@@ -14,12 +14,12 @@ static int	space_checker(char input_position)
 	return (1);
 }
 
-static int	quote_checker(char input_position)
-{
-	if (input_position == '\'' || input_position == '\"')
-		return (1);
-	return (0);
-}
+// static int	quote_checker(char input_position)
+// {
+// 	if (input_position == '\'' || input_position == '\"')
+// 		return (1);
+// 	return (0);
+// }
 
 static char	*meta_separator(char *str, char *input, t_input_utils *in)
 {
@@ -44,23 +44,63 @@ static char	*meta_separator(char *str, char *input, t_input_utils *in)
 	return (str);
 }
 
+// char	*input_separator(char *input)
+// {
+// 	t_input_utils	*in;
+// 	char			*str;
+
+// 	in = ft_calloc(sizeof(t_input_utils), 1);
+// 	str = ft_calloc (sizeof(char *), ft_strlen(input));
+// 	while (input[in->i])
+// 	{
+// 		if (quote_checker(input[in->i]) && !in->open_quotes)
+// 			in->open_quotes = 1;
+// 		else if (quote_checker(input[in->i]) && in->open_quotes)
+// 			in->open_quotes = 0;
+// 		if (input[in->i] == '<' || input[in->i] == '>' || input[in->i] == '|')
+// 			str = meta_separator(str, input, in);
+// 		else if (input[in->i] == SPACE && !in->open_quotes)
+// 			str[in->j] = SEPARATOR;
+// 		else
+// 			str[in->j] = input[in->i];
+// 		in->i++;
+// 		in->j++;
+// 	}
+// 	free(in);
+// 	return (str);
+// }
+
+
+
+
+
+
+
+
 char	*input_separator(char *input)
 {
 	t_input_utils	*in;
 	char			*str;
+	int				db_quotes = 0;
+	int				sp_quotes = 0;
 
 	in = ft_calloc(sizeof(t_input_utils), 1);
 	str = ft_calloc (sizeof(char *), ft_strlen(input));
 	while (input[in->i])
 	{
-		if (quote_checker(input[in->i]) && !in->open_quotes)
-			in->open_quotes = 1;
-		else if (quote_checker(input[in->i]) && in->open_quotes)
-			in->open_quotes = 0;
-		if (input[in->i] == '<' || input[in->i] == '>' || input[in->i] == '|')
-			str = meta_separator(str, input, in);
-		else if (input[in->i] == SPACE && !in->open_quotes)
-			str[in->j] = SEPARATOR;
+		if (input[in->i] == '\'')
+			sp_quotes = !sp_quotes;
+		if (input[in->i] == '\"')
+			db_quotes = !db_quotes;
+		if (!db_quotes && !sp_quotes)
+		{
+			if ((input[in->i] == '<' || input[in->i] == '>' || input[in->i] == '|'))
+				str = meta_separator(str, input, in);
+			else if (input[in->i] == SPACE && !in->open_quotes)
+				str[in->j] = SEPARATOR;
+			else
+				str[in->j] = input[in->i];
+		}
 		else
 			str[in->j] = input[in->i];
 		in->i++;
