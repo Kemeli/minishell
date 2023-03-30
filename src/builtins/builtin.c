7eeb7ad670bb7f08b516aa69174400c55c48b035
kdaine- <kdaiane-@student.42sp.org.br>
2023-03-30 19:39:43 +0200

    fix bug export

--- src/builtins/builtin.c
+++ src/builtins/builtin.c
@@ -25,16 +25,13 @@ int	check_valid_envar(char **cmd, char *msg1, char *msg3, int exp)
 			j++;
 		else if (!status(msg1, cmd[i], msg3, 1))
 			return (0);
-		while (cmd[i][j] && (is_env_char(cmd[i][j]) || cmd[i][j] == '='))
-		{
-			if (cmd[i][j] && cmd[i][j] == '=' && exp)
-				return (1);
-			else if (cmd[i][j] && cmd[i][j] == '=' && !exp)
-				break ;
+		while (cmd[i][j] && (is_env_char(cmd[i][j])))
 			j++;
-		}
-		if (!is_env_char(cmd[i][j]) && cmd[i][j] != '\0')
+		if (!is_env_char(cmd[i][j]) && cmd[i][j] != '\0' && !exp)
 			return (status(msg1, cmd[i], msg3, 1));
+		if (!is_env_char(cmd[i][j]) && exp && cmd[i][j] != '=' && cmd[i][j] != '\0')
+			return (status(msg1, cmd[i], msg3, 1));
+		j = 0;
 	}
 	return (1);
 }
